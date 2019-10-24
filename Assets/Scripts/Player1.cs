769a2766ef0753b114c9b4dfa738cf1ad6eddb14
Jamie <jamiehaddow@hotmail.com>
2019-10-24 20:51:17 +0100

    initial commit
    initial commit

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+
+    [SerializeField]
+    private Wall wall;
+    [SerializeField]
+    private float _mSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wall = GameObject.Find("Wall").GetComponent<Wall>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizonalInput = Input.GetAxis("Horizontal");
+        float verticalInput =  Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizonalInput, 0, verticalInput);
+
+        transform.Translate(direction * _mSpeed * Time.deltaTime);
+    }
+}
