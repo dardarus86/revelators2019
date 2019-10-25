e9281b4cd593f62df31a30df585f54bc27bb2fe0
JAMIE HADDOW <0705082@uad.ac.uk>
2019-10-25 12:12:09 +0100

    rotation player and firing on direction
    rotation on blue player using joystick and when press R2

---
+++ Assets/Scripts/Basic_Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+    private Vector3 direction;
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        transform.Translate(direction * _speed * Time.deltaTime);
+        Destroy(this.gameObject, 2);
+    }
+
+    public void SetDirection(Vector3 dir)
+    {
+        direction = dir;
+    }
+}
--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -9,26 +9,65 @@ public class Player1 : MonoBehaviour
     private Wall wall;
     [SerializeField]
     private float _mSpeed = 5.0f;
-    private float test = 6;
+    [SerializeField]
+    private float _fireRate = 0.2f;
+    [SerializeField]
+    private float _cantFire = -1.0f;
+    [SerializeField]
+    private int _lives = 5;
+    [SerializeField]
+    private SpawnManager _spawnManager;
+    [SerializeField]
+    private GameObject _basicBulletPrefab;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        transform.position = new Vector3(Random.Range(-2.1f, 4.0f), 0.75f, Random.Range(-0.2f, -7.0f));
         wall = GameObject.Find("Wall").GetComponent<Wall>();
+        if(wall == null)
+        {
+            Debug.LogError(" Wall is NULL");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.JoystickButton7) && Time.time > _cantFire)
+        {
+            FireBasicBullet();
+        }
+        calculateRotation();
     }
 
     void CalculateMovement()
     {
-        float horizonalInput = Input.GetAxis("Horizontal");
-        float verticalInput =  Input.GetAxis("Vertical");
+        float LhorizonalInput = Input.GetAxis("LHorizontal");
+        float LverticalInput =  Input.GetAxis("LVertical");
 
-        Vector3 direction = new Vector3(horizonalInput, 0, verticalInput);
+        Vector3 direction = new Vector3(LhorizonalInput, 0, LverticalInput);
 
-        transform.Translate(direction * _mSpeed * Time.deltaTime);
+        transform.Translate(direction * _mSpeed * Time.deltaTime, Space.World);
+    }
+
+    void calculateRotation()
+    {
+        float RhorizontalInput = Input.GetAxis("RHorizontal");
+        float RverticalInput = Input.GetAxis("RVertical");
+        print(RhorizontalInput + ", " + RverticalInput);
+        transform.LookAt(transform.position + new Vector3(RhorizontalInput, 0, RverticalInput), Vector3.up);
+   
+    }
+
+    void FireBasicBullet()
+    {
+        _cantFire = Time.time + _fireRate;
+
+        GameObject newBullet = Instantiate(_basicBulletPrefab, transform.position + transform.forward, Quaternion.identity);
+        newBullet.GetComponent<Basic_Bullet>().SetDirection(transform.forward);
     }
 }
