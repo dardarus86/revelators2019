8ba9178781f8076c009ae1ce37fe6a63d6199d6a
JAMIE HADDOW <0705082@uad.ac.uk>
2019-10-26 13:54:48 +0100

    Audio
    audipo setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,14 @@ public class Player : MonoBehaviour
     [SerializeField]
     private string Rvertical = "RVertical";
     [SerializeField]
+    private string Controller = "0";
+    //[SerializeField]
+    //private string Controller1 = "Controller1";
+    //[SerializeField]
+    //private string Controller2 = "Controller2";
+    //[SerializeField]
+    //private string Controller3 = "Controller3";
+    [SerializeField]
     private Wall wall;
     [SerializeField]
     private float _mSpeed = 5.0f;
@@ -22,7 +30,7 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _cantFire = -1.0f;
     [SerializeField]
-    private int _lives = 3;
+    private int _lives = 100;
     [SerializeField]
     private SpawnManager _spawnManager;
     [SerializeField]
@@ -33,8 +41,16 @@ public class Player : MonoBehaviour
     public GameObject spawn;
     [SerializeField]
     public string _team;
- 
-   
+    [SerializeField]
+    private AudioClip _fireSoundClip;
+    [SerializeField]
+    private AudioClip _deathSoundClip;
+    [SerializeField]
+    private AudioSource _audio;
+
+    private bool _isDead = false;
+
+
 
 
     // Start is called before the first frame update
@@ -43,17 +59,35 @@ public class Player : MonoBehaviour
 
       if (gameObject.tag == "Player1" || gameObject.tag == "Player2")
         {
-           // this.transform.position = new Vector3(Random.Range(-1.1f, 4.0f), 0.75f, Random.Range(-0.2f, -7.0f));
-           // this.transform.position = new Vector3(Random.Range(-1.1f, 4.0f), 3.75f, Random.Range(-0.2f, -7.0f));
-            // transform.position = new Vector3(Random.Range(-2.1f, 4.0f), 0.75f, Random.Range(-0.2f, -7.0f));
+           this.transform.position = new Vector3(Random.Range(-1.1f, 4.0f), 0.75f, Random.Range(-0.2f, -7.0f));
         }
         if (gameObject.tag == "Player3" || gameObject.tag == "Player4")
         {
             this.transform.position = new Vector3(Random.Range(7.37f, 13.3f), 3.75f, Random.Range(-0.63f, -6.1f));
-            // transform.position = new Vector3(Random.Range(-2.1f, 4.0f), 0.75f, Random.Range(-0.2f, -7.0f));
         }
 
-       
+
+        //_shootSource = GetComponent<AudioSource>();
+        //if (_shootSource == null)
+        //{
+        //    Debug.LogError("Audio Source is NULL");
+        //}
+        //else
+        //{
+        //    _shootSource.clip = _fireSoundClip;
+        //}
+        _audio = GetComponent<AudioSource>();
+
+        if (_audio == null)
+        {
+            Debug.LogError("Audio source on player is NULL");
+        }
+        else
+        {
+           _audio.clip = _fireSoundClip;
+        }
+
+
         wall = GameObject.Find("Wall").GetComponent<Wall>();
         if(wall == null)
         {
@@ -66,9 +100,42 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
-        if (Input.GetKey(KeyCode.JoystickButton7) && Time.time > _cantFire)
+        if (Controller == "1")
         {
-            FireBasicBullet();
+            
+            if (Input.GetKey(KeyCode.Joystick1Button7) && Time.time > _cantFire)
+            {
+                print("controller 1 fired" + " " + Input.GetJoystickNames());
+                FireBasicBullet();
+                _audio.Play();
+            }
+        }
+        if (Controller == "2")
+        {
+            if (Input.GetKey(KeyCode.Joystick2Button7) && Time.time > _cantFire)
+            {
+                print("controller 2 fired" + " " + Input.GetJoystickNames());
+                FireBasicBullet();
+                _audio.Play();
+            }
+        }
+        if (Controller == "3")
+        {
+            if (Input.GetKey(KeyCode.Joystick3Button7) && Time.time > _cantFire)
+            {
+                print("controller 3 fired");
+                FireBasicBullet();
+                _audio.Play();
+            }
+        }
+        if (Controller == "4")
+        {
+            if (Input.GetKey(KeyCode.Joystick4Button7) && Time.time > _cantFire)
+            {
+                print("controller 3 fireda");
+                FireBasicBullet();
+                _audio.Play();
+            }
         }
         calculateRotation();
     }
@@ -112,13 +179,19 @@ public class Player : MonoBehaviour
 
     public void damage()
     {
+       
         _lives--;
-
+        
         if (_lives < 1)
         {
+          
             Destroy(this.gameObject);
+            //AudioSource.PlayClipAtPoint(_deathSoundClip, new Vector3(5, 1, 2));
+            
+
         }
     }
 
-   
+
+
 }
